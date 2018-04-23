db63b22ca7c6cb16ee2bdbe6744389ba9c6a9589
Anastasia Goryacheva <tyanas.go@gmail.com>
2018-04-23 20:50:46 +0300

    export trick

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path')
 module.exports = {
     entry: './src/index.js',
     output: {
+        libraryExport: 'default',
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
