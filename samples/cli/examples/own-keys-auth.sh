b6ac155d6bda810359dbb97c104e8953071d9d7c
Anastasia Goryacheva <tyanas.go@gmail.com>
2018-08-06 17:42:52 +0300

    cli: own keys support

--- samples/cli/examples/own-keys-auth.sh
+++ samples/cli/examples/own-keys-auth.sh
@@ -5,4 +5,6 @@ export $(cat .env)
 echo "A simple example for translating a string to Spanish"
 node index.js --key=$INTENTO_API_KEY \
     --to=es \
+    --provider=ai.text.translate.google.translate_api.2-0 \
+    --auth="[{\"key\": \"$YOUR_GOOGLE_APIKEY\" }]" \
     "During simulation, genotypes induce patterns of subsystem activities"
--- src/index.js
+++ src/index.js
@@ -260,11 +260,13 @@ IntentoConnector.prototype.fulfill = function(slug, parameters = {}) {
         pretty_print,
         processing = {},
     } = parameters
+    const providerList = stringToList(provider)
+
     const content = {
         context: { text, from, to, lang, category, format },
         service: {
-            provider: stringToList(provider),
-            auth,
+            provider: providerList,
+            auth: ownCredentials(auth, providerList),
             // prettier-ignore
             'async': asyncMode,
             bidding,
@@ -542,4 +544,55 @@ function stringToList(value) {
     }
 
     return value.split(',').map(s => s.trim())
+}
+
+/**
+ * Form auth object if some custom/own keys were passed
+ * Accepts
+ * - auth="{\"key\": \"$SOME_PROVIDER_APIKEY\" }" - JSON decoded object, any structure accepted
+ * - auth="{\"user\": \"$SOME_PROVIDER_USERNAME\", \"password\": \"$SOME_PROVIDER_PASSWORD\" }" - JSON decoded object, any structure accepted
+ * - auth="[{\"key\": \"$SOME_PROVIDER_APIKEY\" }]" - JSON decoded list of objects, , any structure accepted
+ * - auth="{\"some-provider-id\":[{\"key\": \"$SOME_PROVIDER_APIKEY\" }]}" - JSON decoded full auth object, where keys are provider ids
+ * - auth={'some-provider-id':[{ key: $SOME_PROVIDER_APIKEY }] } - javascript object, where keys are provider ids
+ * @param {string} auth - credential description
+ * @param {array} providerList - list of provider ids as strings
+ * @returns {object} - correct auth object
+ */
+function ownCredentials(auth, providerList) {
+    if (!auth) {
+        return
+    }
+    if (typeof auth === 'object') {
+        return auth
+    }
+
+    let authObj
+    let authKeys
+
+    if (auth[0] === '{') {
+        const authKeysParsed = JSON.parse(auth)
+
+        if (providerList.indexOf(Object.keys(authKeysParsed)[0]) !== -1) {
+            authObj = authKeysParsed
+            // keep authKeys undefined
+        } else {
+            // keep authObj undefined
+            authKeys = [authKeysParsed]
+        }
+    } else if (auth[0] === '[') {
+        authKeys = JSON.parse(auth)
+    }
+
+    if (authKeys) {
+        if (providerList.length > 1) {
+            throw new Error(
+                'Unclear auth parameter: specify one provider or clarify provider inside auth object'
+            )
+        }
+        if (!authObj) {
+            authObj = { [providerList[0]]: authKeys }
+        }
+    }
+
+    return authObj
 }
